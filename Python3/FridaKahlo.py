6abc26115c8eca2a27115e5a3aa8d9a9a4775d90
SteveJarosi <dr.jarosi@yahoo.com>
2021-03-12 10:31:02 +0200

    Frida Kahlo project

---
+++ Python3/FridaKahlo.py
@@ -0,0 +1,12 @@
+paintings = ["The Two Fridas", "My Dress Hangs Here", "Tree of Hope", "Self Portrait With Monkeys"]
+dates = [1939, 1933, 1946, 1940]
+paintings = list(zip(paintings, dates))
+print(paintings)
+paintings.append(("The Broken Column", 1944))
+paintings.append(("The Wounded Deer", 1946))
+paintings.append(("Me and My Doll", 1937))
+print(len(paintings))
+audio_tour_number = [x for x in range(1, len(paintings)+1)]
+print(audio_tour_number)
+master_list = list(zip(audio_tour_number, paintings))
+print(master_list)
