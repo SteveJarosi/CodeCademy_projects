a26661cfc47f50f57067a3559f01ec522b6a936b
SteveJarosi <dr.jarosi@yahoo.com>
2021-03-15 15:12:11 +0200

    Scrabble extended

--- Python3/Scrabble.py
+++ Python3/Scrabble.py
@@ -1,8 +1,13 @@
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
            "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+letters_low = []
+for l in letters:
+    letters_low.append(l.lower())
+print(letters_low)
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1,
           3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 letter_to_points = dict(zip(letters, points))
+letter_to_points.update(dict(zip(letters_low, points)))
 letter_to_points[" "] = 0
 print(letter_to_points)
 
@@ -25,10 +30,31 @@ player_to_words = {'player1': ['BLUE', 'TENNIS', 'EXIT'], 'wordNerd': ['EARTH', 
 
 player_to_points = {}
 
-for player in player_to_words:
-    print(player, player_to_words[player])
-    player_points = 0
-    for word in player_to_words[player]:
-        player_points += score_word(word)
-    player_to_points[player] = player_points
-print(player_to_points)
+
+def update_point_totals():
+    for player in player_to_words:
+        print(player, player_to_words[player])
+        player_points = 0
+        for word in player_to_words[player]:
+            player_points += score_word(word)
+        player_to_points[player] = player_points
+    print(player_to_points)
+
+
+update_point_totals()
+
+
+def play_word(player, word):
+    if player in player_to_words:
+        player_to_words[player].append(word)
+    else:
+        player_to_words.update({player: [word]})
+
+
+play_word('player1', 'TIGER')
+play_word('player2', 'EGGBERT')
+play_word('player2', 'cartwheel')
+
+player_to_points = {}
+
+update_point_totals()
