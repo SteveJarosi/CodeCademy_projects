59578372d98f5816a5da6246cec2df1158632a4e
SteveJarosi <dr.jarosi@yahoo.com>
2021-03-18 10:37:41 +0200

    Boredless Tourist project

---
+++ Python3/Boredless_Tourist.py
@@ -0,0 +1,9 @@
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+def get_destination_index(destination):
+  destination_index = destinations.index(destination)
+  return destination_index
+print(get_destination_index("Los Angeles, USA"))
+
+def get_traveler_location():
+    pass
