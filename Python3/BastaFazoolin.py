d3b8a27a0857ab60ee4285e4e00c980b977adb69
SteveJarosi <dr.jarosi@yahoo.com>
2021-03-23 12:43:50 +0200

    Basta Fazoolin' project in progress

--- Python3/BastaFazoolin.py
+++ Python3/BastaFazoolin.py
@@ -6,8 +6,31 @@ class Menu:
     self.items = items
     self.start_time = start_time
     self.end_time = end_time
+  def __repr__(self):
+    return self.name.title() + " menu is available from " + str(self.start_time)[:-3] + " to " + str(self.end_time)[:-3]
+  def calculate_bill(self, purchased_items):
+    total = 0
+    for item in purchased_items:
+      total+= int(self.items[item])
+    return total
 
 brunch = Menu('brunch', {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
-}, time(11,0), time(16,0))
-print(brunch.start_time)
+}, time(11), time(16))
+
+early_bird = Menu('early_bird', {
+  'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+}, time(15), time(18))
+
+dinner = Menu('dinner', {
+  'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
+}, time(17), time(23))
+
+kids = Menu('kids', {
+  'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
+}, time(11), time(21))
+
+print(brunch)
+
+print(brunch.calculate_bill(['pancakes', 'home fries', 'coffee']))
+print(early_bird.calculate_bill(['salumeria plate', 'mushroom ravioli (vegan)']))
