6bdff68930fd7207bea579a0323108b4da581a9d
SteveJarosi <dr.jarosi@yahoo.com>
2021-03-25 14:45:01 +0200

    Medical Insurance 8 start

---
+++ Python3/Medical_Insurance8.py
@@ -0,0 +1,7 @@
+# Add your code here
+medical_costs = { "Marina": 6607.0,
+                  "Vinay": 3225.0}
+medical_costs.update({"Connie": 8886.0, "Isaac": 16444.0, "Valentina": 6420.0})
+print(medical_costs)
+medical_costs.update({"Vinay": 3325.0})
+print(medical_costs)
