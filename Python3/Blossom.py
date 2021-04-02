67d5a906a4135bd91fd5578620709f4852085175
SteveJarosi <dr.jarosi@yahoo.com>
2021-04-02 10:32:46 +0300

    done with bugs Blossom project

--- Python3/Blossom.py
+++ Python3/Blossom.py
@@ -1,10 +1,38 @@
+from linked_list import Node, LinkedList
+from blossom_lib import flower_definitions
+
 class HashMap:
   def __init__(self, size):
     self.array_size = size
-    self.array = [None for x in range(size)]
+    self.array = [LinkedList() for x in range(self.array_size)]
   def hash(self, key):
-    return sum(key.encode)
+    return sum(key.encode())
   def compress(self, hash_code):
-    return hash_code % self.array.size
+    return hash_code % self.array_size
   def assign(self, key, value):
-    pass
+    array_index = self.compress(self.hash(key))
+    payload = Node([key, value])
+    list_at_array = self.array[array_index]
+    for n in list_at_array:
+      if n[0] == key:
+        n[1] = value
+        return
+    list_at_array.insert(payload)
+    
+  def retrieve(self, key):
+    array_index = self.compress(self.hash(key))
+    list_at_index = self.array[array_index]
+    for n in list_at_index:
+      if n[0] == key:
+        return n[1]
+      else:
+        return None
+
+blossom = HashMap(len(flower_definitions))
+for i in flower_definitions:
+  blossom.assign(i[0], i[1])
+
+print(blossom.retrieve('daisy'))
+
+for i in flower_definitions:
+  print(blossom.retrieve(i[0]))
