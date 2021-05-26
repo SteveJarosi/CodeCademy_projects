a0497eaca747c754292060fa8f681ac972ea1815
SteveJarosi <dr.jarosi@yahoo.com>
2021-05-26 16:06:18 +0300

    Finish Animal Fun Facts

--- Animal Fun Facts/app.compiled.js
+++ Animal Fun Facts/app.compiled.js
@@ -6,15 +6,15 @@
   });
   var animals = exports.animals = {
     dolphin: {
-      image: '/images/dolphin.jpg',
+      image: './images/dolphin.jpg',
       facts: ['Dolphins have been shown to give distinct names to each other!', 'Dolphins are known to display their own culture!', 'Dolphins have two stomachs!']
     },
     lobster: {
-      image: '/images/lobster.jpg',
+      image: './images/lobster.jpg',
       facts: ['Lobsters taste with their legs!', 'Lobsters chew with their stomachs!', 'Lobsters can live as long as 100 years.']
     },
     starfish: {
-      image: '/images/starfish.jpg',
+      image: './images/starfish.jpg',
       facts: ['Starfish can have up to 40 arms!', 'Starfish have no brain and no blood!', 'Starfish can regenerate their own arms!']
     }
   };
@@ -35,16 +35,18 @@
   function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
   
   var title = "";
+  var showBackground = true;
   var background = _react2.default.createElement('img', { className: 'background', alt: 'ocean', src: './images/ocean.jpg' });
   var images = [];
   for (var animal in _animals.animals) {
-    images.push(_react2.default.createElement('img', { key: animal, className: 'animal', alt: animal, src: "."+_animals.animals[animal].image, 'aria-label': animal, role: 'button' }));
+    images.push(_react2.default.createElement('img', { onClick: displayFact, key: animal, className: 'animal', alt: animal, src: _animals.animals[animal].image, 'aria-label': animal, role: 'button' }));
   };
   //console.log(images);
   var animalFacts = _react2.default.createElement(
     'div',
     null,
-    background,
+    showBackground && background,
+    _react2.default.createElement('p', { id: 'fact' }),
     _react2.default.createElement(
       'div',
       { className: 'animals' },
@@ -53,10 +55,17 @@
     _react2.default.createElement(
       'h1',
       null,
-      title ? title : 'Click an animal for a fun fact'
+      title || 'Click an animal for a fun fact'
     )
   );
   _reactDom2.default.render(animalFacts, document.getElementById('root'));
+  
+  function displayFact(e) {
+    var animal = e.target.alt;
+    var randIndex = Math.floor(Math.random() * _animals.animals[animal].facts.length);
+    var funFact = _animals.animals[animal].facts[randIndex];
+    document.getElementById('fact').innerHTML = funFact;
+  }
   
   },{"./animals":1,"react":undefined,"react-dom":undefined}]},{},[2]);
   
--- Animal Fun Facts/app.js
+++ Animal Fun Facts/app.js
@@ -3,20 +3,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const title = "";
-const background = <img className='background' alt='ocean' src='/images/ocean.jpg'/>
+const showBackground = true;
+const background = <img className='background' alt='ocean' src='./images/ocean.jpg' />;
 const images = [];
 for (const animal in animals) {
-  images.push(<img key={animal} className='animal' alt={animal} src= {animals[animal].image} aria-label={animal} role='button' />)
+    images.push(<img onClick={displayFact} key={animal} className='animal' alt={animal} src={animals[animal].image} aria-label={animal} role='button' />)
 };
-//console.log(images);
 const animalFacts = (
-  <div>
-  {background}
-  <div className='animals'>
-  {images}
-  </div>
-  <h1>{title ? title : 'Click an animal for a fun fact'}</h1>
-  </div>
+    <div>
+        {showBackground && background}
+        <p id='fact'></p>
+        <div className='animals'>
+            {images}
+        </div>
+        <h1>{title || 'Click an animal for a fun fact'}</h1>
+    </div>
 );
 ReactDOM.render(animalFacts, document.getElementById('root'));
 
+function displayFact(e) {
+    let animal = e.target.alt;
+    let randIndex = Math.floor(Math.random() * animals[animal].facts.length);
+    let funFact = animals[animal].facts[randIndex];
+    document.getElementById('fact').innerHTML = funFact;
+}
